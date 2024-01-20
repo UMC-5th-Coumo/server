07960313ab639950d3c7dac8b2280c4b954142e1
Kang Hyun Woo <gusdn741021@gmail.com>
2024-01-20 20:49:24 +0900

    [Refactor/#3]: 스웨거 화면 설명 관련 코드 수정

--- server/src/main/java/coumo/server/config/SwaggerConfig.java
+++ server/src/main/java/coumo/server/config/SwaggerConfig.java
@@ -33,19 +33,19 @@ public class SwaggerConfig {
         prodServer.setUrl(prodUrl);
         prodServer.setDescription("Server URL in Production environment");
 
-        Contact contact = new Contact();
-        contact.setEmail("bezkoder@gmail.com");
-        contact.setName("BezKoder");
-        contact.setUrl("https://www.bezkoder.com");
+//        Contact contact = new Contact();
+//        contact.setEmail("temp@gmail.com");
+//        contact.setName("temp");
+//        contact.setUrl("https://www.temp.com");
 
-        License mitLicense = new License().name("MIT License").url("https://choosealicense.com/licenses/mit/");
+//        License mitLicense = new License().name("MIT License").url("https://choosealicense.com/licenses/mit/");
 
         Info info = new Info()
-                .title("Tutorial Management API")
+                .title("Coumo API")
                 .version("1.0")
-                .contact(contact)
-                .description("This API exposes endpoints to manage tutorials.").termsOfService("https://www.bezkoder.com/terms")
-                .license(mitLicense);
+//                .contact(contact)
+                .description("This API exposes endpoints to manage Coumo.").termsOfService("http://localhost:8080/");
+//                .license(mitLicense);
 
         return new OpenAPI().info(info).servers(List.of(devServer, prodServer));
     }
