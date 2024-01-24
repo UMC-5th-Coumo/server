1752d7d0fd1a4180e51dbde6d3f5754d18c045e7
Kang Hyun Woo <gusdn741021@gmail.com>
2024-01-24 20:03:32 +0900

    [Feat/#10]: 사장님 웹 controller 작성

---
+++ server/src/main/java/coumo/server/web/controller/StoreRestController.java
@@ -0,0 +1,90 @@
+package coumo.server.web.controller;
+
+import coumo.server.apiPayload.ApiResponse;
+import coumo.server.web.dto.StoreRequestDTO;
+import coumo.server.web.dto.StoreResponseDTO;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.Parameters;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/owner/store")
+public class StoreRestController {
+
+    @GetMapping("/{storeId}/basic")
+    @Operation(summary = "사장님이 작성한 가게 정보(기본 정보) 조회 API",
+            description = "사장님이 가게 정보를 저장한 정보(이제 막 회원가입한 유저하면 빈 값)를 조회하는 API입니다. 가게Id를 알려주세요.")
+    @ApiResponses({
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "COMMON200",description = "OK, 성공"),
+//            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "AUTH003", description = "access 토큰을 주세요!",content = @Content(schema = @Schema(implementation = ApiResponse.class))),
+    })
+    @Parameters({
+            @Parameter(name = "storeId", description = "가게 아이디, path variable 입니다!"),
+    })
+    public ApiResponse<StoreResponseDTO.resultBasicDTO> getBasic(
+            @PathVariable("storeId") Long storeId){
+
+        return ApiResponse.onSuccess(StoreResponseDTO.resultBasicDTO.builder().build());
+    }
+
+    @GetMapping("/{storeId}/detail")
+    @Operation(summary = "사장님이 작성한 가게 정보(매장 설명) 조회 API",
+            description = "사장님이 가게 정보를 저장한 정보(이제 막 회원가입한 유저하면 빈 값)를 조회하는 API입니다. 가게Id를 알려주세요.")
+    @ApiResponses({
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "COMMON200",description = "OK, 성공"),
+//            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "AUTH003", description = "access 토큰을 주세요!",content = @Content(schema = @Schema(implementation = ApiResponse.class))),
+    })
+    @Parameters({
+            @Parameter(name = "storeId", description = "가게 아이디, path variable 입니다!"),
+    })
+    public ApiResponse<StoreResponseDTO.resultDetailDTO> getDetail(
+            @PathVariable("storeId") Long storeId){
+
+        return ApiResponse.onSuccess(StoreResponseDTO.resultDetailDTO.builder().build());
+    }
+
+    @PatchMapping("/{storeId}/basic")
+    @Operation(summary = "사장님이 작성한 가게 정보(기본 정보) 저장 API",
+            description = "사장님이 가게 정보를 저장한 정보를 저장하는 API입니다. 가게에 대한 정보를 json으로 보내주세요.")
+    @ApiResponses({
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "COMMON200",description = "OK, 성공"),
+//            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "AUTH003", description = "access 토큰을 주세요!",content = @Content(schema = @Schema(implementation = ApiResponse.class))),
+    })
+    @Parameters({
+            @Parameter(name = "updateBasicDTO", description = "가게에 대한 정보가 담긴 json"),
+    })
+    public ApiResponse<Long> updateBasic(@RequestBody StoreRequestDTO.updateBasicDTO updateBasicDTO){
+        return ApiResponse.onSuccess(0L);
+    }
+
+    @PatchMapping("/{storeId}/detail")
+    @Operation(summary = "사장님이 작성한 가게 정보(매장 설명) 저장 API",
+            description = "사장님이 가게 정보를 저장한 정보를 저장하는 API입니다. 가게에 대한 정보를 FormData으로 보내주세요.")
+    @ApiResponses({
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "COMMON200",description = "OK, 성공"),
+//            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "AUTH003", description = "access 토큰을 주세요!",content = @Content(schema = @Schema(implementation = ApiResponse.class))),
+    })
+    @Parameters({
+            @Parameter(name = "storeId", description = "가게 아이디, path variable 입니다!"),
+            @Parameter(name = "storeImages", description = "가게 메인 이미지들 입니다!"),
+            @Parameter(name = "menuImages", description = "메뉴의 이미지들입니다!"),
+            @Parameter(name = "description", description = "가게 설명입니다!!"),
+            @Parameter(name = "menuDetail", description = "가게 메뉴 이름과 설명(가격)이 담긴 객체 배열입니다!"),
+    })
+    public ApiResponse<Long> updateDetail(
+            @RequestParam("storeImages")MultipartFile[] storeImages,
+            @RequestParam("menuImages")MultipartFile[] menuImages,
+            @RequestParam("description")String description,
+            @RequestParam("menuDetail")StoreRequestDTO.MenuDetail[] menuDetail
+            ){
+        return ApiResponse.onSuccess(0L);
+    }
+
+}
--- server/src/main/java/coumo/server/web/dto/StoreRequestDTO.java
+++ server/src/main/java/coumo/server/web/dto/StoreRequestDTO.java
@@ -2,6 +2,7 @@ package coumo.server.web.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import java.util.List;
@@ -9,16 +10,27 @@ import java.util.List;
 public class StoreRequestDTO {
 
     //가게 수정(기본 정보)
-    @Builder
-    @AllArgsConstructor
-    @NoArgsConstructor
-    public static class updateDTO{
+    @Getter
+    public static class updateBasicDTO{
         public String name;
-        public List<String> time;
+        public List<time> time;
         public String telePhone;
         public String category;
         public String location;
         public String longitude;
         public String latitude;
+    }
+
+    @Getter
+    public static class time{
+        String day;
+        String startTime;
+        String endTime;
+    }
+
+    @Getter
+    public static class MenuDetail{
+        String name;
+        String description;
     }
 }
