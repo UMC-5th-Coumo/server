edf8a3a6ca46427d929d6d084cff29efadb5e068
smyoo <proysm5154@naver.com>
2024-02-05 18:56:35 +0900

    [Test] CORS 뚤기

---
+++ src/main/java/coumo/server/config/WebConfig.java
@@ -0,0 +1,18 @@
+package coumo.server.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry){
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000", "https://dev.coumo.shop") // 허용할 출처
+                .allowedMethods("GET", "POST", "PUT", "DELETE") // 허용할 HTTP method
+                .allowCredentials(true) // 쿠키 인증 요청 허용
+                .maxAge(3000); // 원하는 시간만큼 pre-flight 리퀘스트를 캐싱
+    }
+}
