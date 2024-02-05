1782157b4bf3cc48870b52082196e913cbde0d58
Kang Hyun Woo <gusdn741021@gmail.com>
2024-02-05 20:05:53 +0900

    [Refactor]: yml 수정

--- src/main/java/coumo/server/config/WebConfig.java
+++ src/main/java/coumo/server/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry){
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:8080", "https://dev.coumo.shop") // 허용할 출처
+                .allowedOrigins("http://localhost:8080", "https://dev.coumo.shop/**", "https://dev.coumo.shop/swagger-ui/index.html") // 허용할 출처
                 .allowedMethods("GET", "POST", "PUT", "DELETE") // 허용할 HTTP method
                 .allowCredentials(true) // 쿠키 인증 요청 허용
                 .maxAge(3000); // 원하는 시간만큼 pre-flight 리퀘스트를 캐싱
