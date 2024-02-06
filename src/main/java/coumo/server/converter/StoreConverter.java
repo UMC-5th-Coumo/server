d742a1bb0945da08923b9258ed0d85c59c93846d
Kang Hyun Woo <gusdn741021@gmail.com>
2024-02-06 23:09:55 +0900

    [Fix/#55]: StoreConverter 리팩토링

--- src/main/java/coumo/server/converter/StoreConverter.java
+++ src/main/java/coumo/server/converter/StoreConverter.java
@@ -1,65 +1,65 @@
 package coumo.server.converter;
 
-import coumo.server.apiPayload.exception.handler.StoreHandler;
+import coumo.server.domain.Menu;
 import coumo.server.domain.Store;
+import coumo.server.domain.StoreImage;
+import coumo.server.domain.Timetable;
 import coumo.server.web.dto.StoreResponseDTO;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import java.util.stream.Collectors;
 
+@Slf4j
 public class StoreConverter {
-    public static StoreResponseDTO.StoreBasicDTO toResultBasicDTO(Store store){
+    public static StoreResponseDTO.StoreBasicDTO toResultBasicDTO(Store store, List<Timetable> timetableList){
         StoreResponseDTO.StoreBasicDTO storeBasicDTO = StoreResponseDTO.StoreBasicDTO.builder()
-                                                            .name(store.getName())
-                                                            .telePhone(store.getTelephone())
-                                                            .longitude(String.valueOf(store.getPoint().getX()))
-                                                            .latitude(String.valueOf(store.getPoint().getY()))
-                                                            .time(new ArrayList<>())
-                                                            .location(store.getStoreLocation())
-                                                            .category(store.getStoreType().toString())
-                                                            .build();
-        store.getTimetableList().stream()
-                .map(item -> storeBasicDTO.getTime()
-                        .add(new StoreResponseDTO.TimeInfo(item.getDay(), item.getStartTime(), item.getEndTime())))
-                .collect(Collectors.toList());
+                .name(store.getName())
+                .telePhone(store.getTelephone())
+                .longitude(String.valueOf(store.getPoint().getX()))
+                .latitude(String.valueOf(store.getPoint().getY()))
+                .time(new ArrayList<>())
+                .location(store.getStoreLocation())
+                .category(store.getStoreType().toString())
+                .build();
+
+        if (timetableList != null) {
+            timetableList.forEach(item -> storeBasicDTO.getTime()
+                    .add(new StoreResponseDTO.TimeInfo(item.getDay(), item.getStartTime(), item.getEndTime())));
+        }
 
         return storeBasicDTO;
     }
 
-    public static StoreResponseDTO.StoreDetailDTO toResultDetailDTO(Store store){
+    public static StoreResponseDTO.StoreDetailDTO toResultDetailDTO(Store store, List<StoreImage> storeImageList, List<Menu> menuList){
         StoreResponseDTO.StoreDetailDTO storeDetailDTO = StoreResponseDTO.StoreDetailDTO.builder()
-                                                                .storeImages(new ArrayList<>())
-                                                                .menus(new ArrayList<>())
-                                                                .description(store.getStoreDescription())
-                                                                .build();
-        store.getStoreImageList().stream().
-                map(item -> storeDetailDTO.getStoreImages().add(item.getStoreImage()))
-                .collect(Collectors.toList());
+                .storeImages(new ArrayList<>())
+                .menus(new ArrayList<>())
+                .description(store.getStoreDescription())
+                .build();
 
-        store.getMenuList().stream().
-                map(item -> storeDetailDTO.getMenus()
-                        .add(new StoreResponseDTO.MenuInfo(item.getName(), item.getMenuImage(), item.getMenuDescription(), item.getIsNew())))
-                .collect(Collectors.toList());
+        if (storeImageList != null) storeImageList.forEach(item -> storeDetailDTO.getStoreImages().add(item.getStoreImage()));
+
+        if (menuList != null) menuList.forEach(item -> storeDetailDTO.getMenus()
+                .add(new StoreResponseDTO.MenuInfo(item.getName(), item.getMenuImage(), item.getMenuDescription(), item.getIsNew())));
+
         return storeDetailDTO;
     }
+
 
     public static List<StoreResponseDTO.FamousStoreDTO> toFamousStoreDTO(List<StoreResponseDTO.StoreStampInfo> storeStampInfoList){
         if(storeStampInfoList.isEmpty()) return Collections.emptyList();
 
-        //이미지 개수 예외처리 시급함
         List<StoreResponseDTO.FamousStoreDTO> resultList = new ArrayList<>();
-        storeStampInfoList.stream()
-                .map(item -> resultList.add(StoreResponseDTO.FamousStoreDTO.builder()
-                        .storeId(item.getStore().getId())
-                        .name(item.getStore().getName())
-                        .description(item.getStore().getStoreDescription())
-                        .location(item.getStore().getStoreLocation())
-                        .storeImage(item.getStore().getStoreImageList().get(0).getStoreImage())
-                        .build()))
-                .collect(Collectors.toList());
+        storeStampInfoList.forEach(item -> resultList.add(StoreResponseDTO.FamousStoreDTO.builder()
+                .storeId(item.getStore().getId())
+                .name(item.getStore().getName())
+                .description(item.getStore().getStoreDescription())
+                .location(item.getStore().getStoreLocation())
+                .storeImage(item.getStore().getStoreImageList().get(0).getStoreImage())
+                .build()));
 
         return resultList;
     }
@@ -69,16 +69,13 @@ public class StoreConverter {
 
         List<StoreResponseDTO.NearestStoreDTO> resultList = new ArrayList<>();
         List<Store> storeList = storePage.getContent();
-        //이미지 개수 예외처리 시급함
-        storeList.stream()
-                .map(item -> resultList.add(StoreResponseDTO.NearestStoreDTO.builder()
-                                .storeId(item.getId())
-                                .storeImage(item.getStoreImageList().get(0).getStoreImage())
-                                .location(item.getStoreLocation())
-                                .couponCnt(item.getCustomerCouponLength(customerId))
-                                .name(item.getName())
-                        .build()))
-                .collect(Collectors.toList());
+        storeList.forEach(item -> resultList.add(StoreResponseDTO.NearestStoreDTO.builder()
+                .storeId(item.getId())
+                .storeImage(item.getStoreImageList().get(0).getStoreImage())
+                .location(item.getStoreLocation())
+                .couponCnt(item.getCustomerCouponLength(customerId))
+                .name(item.getName())
+                .build()));
 
         return resultList;
     }
@@ -99,18 +96,14 @@ public class StoreConverter {
                 .menus(new ArrayList<>())
                 .build();
 
-        store.getStoreImageList().stream()
-                .map(item->result.getImages().add(item.getStoreImage()))
-                .collect(Collectors.toList());
+        store.getStoreImageList().forEach(item -> result.getImages().add(item.getStoreImage()));
 
-        store.getMenuList().stream()
-                .map(item->result.getMenus().add(StoreResponseDTO.MenuInfo.builder()
-                                .name(item.getName())
-                                .description(item.getMenuDescription())
-                                .image(item.getMenuImage())
-                                .isNew(item.getIsNew())
-                        .build()))
-                .collect(Collectors.toList());
+        store.getMenuList().forEach(item -> result.getMenus().add(StoreResponseDTO.MenuInfo.builder()
+                .name(item.getName())
+                .description(item.getMenuDescription())
+                .image(item.getMenuImage())
+                .isNew(item.getIsNew())
+                .build()));
 
         return result;
     }
