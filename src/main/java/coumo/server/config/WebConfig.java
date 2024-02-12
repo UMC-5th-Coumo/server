bb2576245b7d0fd041f74265f07fd353446fbbbd
Kang Hyun Woo <gusdn741021@gmail.com>
2024-02-12 23:03:24 +0900

    [Fix]: :bug: CORS 수정

--- src/main/java/coumo/server/config/WebConfig.java
+++ src/main/java/coumo/server/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry){
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:8080", "https://dev.coumo.shop", "https://coumo-develop.netlify.app") // 허용할 출처
+                .allowedOrigins("http://localhost:8080", "https://dev.coumo.shop", "https://coumo-develop.netlify.app", "https://resttesttest.com/") // 허용할 출처
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH") // 허용할 HTTP method
                 .allowCredentials(true) // 쿠키 인증 요청 허용
                 .maxAge(3000); // 원하는 시간만큼 pre-flight 리퀘스트를 캐싱
