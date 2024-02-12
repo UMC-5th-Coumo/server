72d5cf9b7cf7dc5643289843387407241a837a33
Kang Hyun Woo <gusdn741021@gmail.com>
2024-02-12 22:11:49 +0900

    [Fix]: :bug:  CORS 수정

--- src/main/java/coumo/server/web/controller/RootController.java
+++ src/main/java/coumo/server/web/controller/RootController.java
@@ -1,8 +1,10 @@
 package coumo.server.web.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin("http://localhost:8080")
 @RestController
 public class RootController {
     @GetMapping("/health")
