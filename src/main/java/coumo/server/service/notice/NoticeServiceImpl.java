5aded374f3f517489a7a829f95e67860fa0155af
smyoo <proysm5154@naver.com>
2024-02-17 20:02:26 +0900

    [Fix/#85] 동네소식 오타

--- src/main/java/coumo/server/service/notice/NoticeServiceImpl.java
+++ src/main/java/coumo/server/service/notice/NoticeServiceImpl.java
@@ -20,7 +20,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.multipart.MultipartFile;\
+import org.springframework.web.multipart.MultipartFile;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
