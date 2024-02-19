6b8777c4c46e272a68b97fdd888051458e4775b4
Willy <willy_1020@naver.com>
2024-02-20 00:46:01 +0900

    [FIx/#136] 쿠폰 UI 서비스 신청 및 신청내역

---
+++ src/main/java/coumo/server/domain/enums/ReceiptState.java
@@ -0,0 +1,7 @@
+package coumo.server.domain.enums;
+
+public enum ReceiptState {
+    APPLIED, //서비스 신청
+    WORKING, //작업 중
+    COMPLETED //작업 완료
+}
--- src/main/java/coumo/server/web/controller/DesignReceiptController.java
+++ src/main/java/coumo/server/web/controller/DesignReceiptController.java
@@ -3,14 +3,20 @@ package coumo.server.web.controller;
 import coumo.server.apiPayload.ApiResponse;
 import coumo.server.converter.DesignReceiptConverter;
 import coumo.server.domain.DesignReceipt;
+import coumo.server.domain.enums.ReceiptState;
 import coumo.server.service.design.DesignReceiptService;
 import coumo.server.service.owner.OwnerService;
 import coumo.server.web.dto.DesignReceiptRequestDTO;
+import coumo.server.web.dto.DesignReceiptServiceResponseDTO;
+import coumo.server.web.dto.DesignReceiptStateChangeRequestDTO;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.Parameters;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequiredArgsConstructor
@@ -30,5 +36,26 @@ public class DesignReceiptController {
             DesignReceipt savedReceipt = designReceiptService.createDesignReceipt(designReceipt);
             return ApiResponse.onSuccess(DesignReceiptConverter.toResponseDTO(savedReceipt));
         }).orElseGet(() -> ApiResponse.onFailure("404", "사용자를 찾을 수 없습니다.",null));
+    }
+
+    @GetMapping("/{ownerId}/receipts")
+    @Operation(summary = "쿠폰 UI 서비스 신청내역 보기")
+    public ApiResponse<List<DesignReceiptServiceResponseDTO>> getDesignReceiptsByOwner(@PathVariable Long ownerId) {
+        List<DesignReceiptServiceResponseDTO> response = designReceiptService.findAllDesignReceiptsByOwnerId(ownerId);
+        if(!response.isEmpty()){
+            return ApiResponse.onSuccess(response);
+        } else {
+            return ApiResponse.onFailure("404", "사용자를 찾을 수 없습니다.", null);
+        }
+    }
+
+    @PostMapping("/{ownerId}/receipts/{receiptId}/change-state")
+    @Operation(summary = "쿠폰 UI 서비스 영수증 작업상태 변경하기" , description = "APPLIED:신청접수, WORKING:작업중, COMPLETED:작업완료")
+    public ApiResponse<?> changeReceiptState(
+            @PathVariable Long ownerId,
+            @PathVariable Long receiptId,
+            @RequestBody DesignReceiptStateChangeRequestDTO requestDTO) {
+        designReceiptService.changeDesignReceiptState(receiptId, requestDTO.getState());
+        return ApiResponse.onSuccess("state가 " + requestDTO.getState().name() + "로 변경되었습니다.");
     }
 }
